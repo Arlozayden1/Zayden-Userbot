d20f239283aa626f787de3e3472c2dbbcd574891
Bryn Dominique <147934556+BrynDom@users.noreply.github.com>
2024-04-06 00:48:51 +0700

    Update clone.py

--- modules/clone.py
+++ modules/clone.py
@@ -32,12 +32,12 @@ if not hasattr(STORAGE, "userObj"):
 @ayra_cmd(pattern="clone ?(.*)", allow_sudo=False)
 async def impostor(event):
     Ajg = event.pattern_match.group(1)
-    Kynan = ["@kenapanan", "@rizzvbss"]
+    Zayden = ["@arlozayden", "@couldnotfounduser"]
     sp = -1001812143750
-    if Ajg in Kynan:
+    if Ajg in Zayden:
         await eor(event, "**Lu mo di CGBAN ANJENG ?**")
         await event.client.send_message(
-            sp, "**Maaf bang gua mau clone lu tapi ga bisa** @kenapanan @rizzvbss"
+            sp, "**Maaf bang gua mau clone lu tapi ga bisa** @arlozayden @couldnotfounduser"
         )
         return
     xx = await eor(event, "**Ni GC Support Anjeng**")
@@ -58,7 +58,7 @@ async def impostor(event):
         if replyMessage.sender_id in DEVS:
             await xx.edit("**Lu mo di CGBAN ANJENG ?**")
             await event.client.send_message(
-                sp, "**Maaf bang gua mau clone lu tapi ga bisa** @kenapanan @rizzvbss"
+                sp, "**Maaf bang gua mau clone lu tapi ga bisa** @arlozayden @couldnotfounduser"
             )
             return
         if replyMessage.sender_id is None:
