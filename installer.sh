e8e6badc3164a2eb7d3a9067dd1c94d7d6ce75fb
Bryn Dominique <147934556+BrynDom@users.noreply.github.com>
2024-04-06 03:00:32 +0700

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-REPO="https://github.com/BrynDom/Zayden-Userbot.git"
-DIR="/root/BrynDom"
+REPO="https://github.com/naya1503/Naya-Userbot.git"
+DIR="/root/naya1503"
 
 spinner(){
     local pid=$!
@@ -31,7 +31,7 @@ clone_repo(){
             fi
             return
     fi
-    echo -e "Cloning Zayden-Userbot ${BRANCH}... "
+    echo -e "Cloning Naya-Userbot ${BRANCH}... "
     git clone -b $BRANCH $REPO $DIR
 }
 
