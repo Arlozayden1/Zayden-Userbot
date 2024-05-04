9ae61ca251589fd056184a342de6241eb0a8358a
Bryn Dominique <147934556+BrynDom@users.noreply.github.com>
2024-05-04 19:13:01 +0700

    Update convert.py

--- modules/convert.py
+++ modules/convert.py
@@ -1,5 +1,5 @@
-# Ayra - UserBot
-# Copyright (C) 2021-2022 senpai80
+# Zayden - UserBot
+# Copyright (C) 2021-2022
 #
 # This file is a part of < https://github.com/senpai80/Ayra/ >
 # PLease read the GNU Affero General Public License in
